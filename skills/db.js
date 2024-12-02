d27937efd9b6fcf0c9da8385e24be85a47c8ea0a
leshcsr <a01708987@tec.mx>
2024-12-02 09:57:52 -0600

    change db conection

--- skills/db.js
+++ skills/db.js
@@ -3,19 +3,10 @@ const mongoose = require('mongoose');
 const uri = "mongodb+srv://usersistemasweb:Lsm9WyqtPMpMwH@swdatabase.ajvml.mongodb.net/swdatabase?retryWrites=true&w=majority";
 //const uri = "mongodb+srv://usersistemasweb:Lsm9WyqtPMpMwH@swdatabase.ajvml.mongodb.net/swdatabase?retryWrites=true&w=majority";
 
-const clientOptions = { 
-    useNewUrlParser: true, 
-    useUnifiedTopology: true, 
-    serverApi: { 
-      version: '1', 
-      strict: true, 
-      deprecationErrors: true,
-    } };
-
 async function connectDB() {
   try {
     // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
-    await mongoose.connect(uri, clientOptions);
+    await mongoose.connect(uri);
     await mongoose.connection.db.admin().command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } catch(err) {
