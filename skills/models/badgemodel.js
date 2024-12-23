3412751fb88efbca35c347150d3c970f869eed62
leshcsr <a01708987@tec.mx>
2024-12-23 12:22:23 -0600

    refactor: edit-badges sin validaciones

--- skills/models/badgemodel.js
+++ skills/models/badgemodel.js
@@ -14,22 +14,10 @@ const badgeSchema = new mongoose.Schema({
     type: Number,
     required: true, 
     min: 0, 
-    validate: {
-      validator: function (value) {
-        return value >= this.bitpoints_min;
-      },
-      message: 'bitpoints_max debe ser mayor o igual a bitpoints_min',
-    },
   },
   png: {
     type: String,
     required: true,
-    validate: {
-      validator: function (url) {
-        return /^https?:\/\/.+\.(png|jpg|jpeg|svg)$/i.test(url);
-      },
-      message: 'El campo png debe ser una URL válida de una imagen',
-    },
   },
 },
 );
